5cccfd2206e71c59ac2c7462c99616d8dc304b70
t-mercier <timothee@mercier.app>
2025-09-17 01:22:44 +0200

    remove scripts and stuff

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client']
-  },
+  serverExternalPackages: ['@prisma/client'],
   images: {
-    domains: ['localhost'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
       },
     ],
-  },
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
   },
 };
 
